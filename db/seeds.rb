30de05a3a40c6190a82ec58c5c54c53dea68d37f
Merlin Hannemann <merlinhannemann@gmx.de>
2018-11-05 20:32:18 +0800

    model and controller

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Ingredient.create(name: "lemon")
+Ingredient.create(name: "ice")
+Ingredient.create(name: "mint leaves")
+Ingredient.create(name: "lime")
+Ingredient.create(name: "coke")
+Ingredient.create(name: "soda")
+Ingredient.create(name: "vodka")
+Ingredient.create(name: "rum")
+Ingredient.create(name: "gin")
+Ingredient.create(name: "tonic")
+Ingredient.create(name: "whisky")
+Ingredient.create(name: "OJ")
+
+
+Cocktail.create(name: "Caipirinha")
+Cocktail.create(name: "Mojito")
